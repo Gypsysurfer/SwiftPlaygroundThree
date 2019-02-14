e925df1b1478e9d4543f414111f9b67647056d72
Sergio San Pedro <sergio.sanpedro@me.com>
2019-02-14 10:31:13 -1000

    completed sp #3

--- SwifdPlayground3.playground/Contents.swift
+++ SwifdPlayground3.playground/Contents.swift
@@ -1,3 +1,34 @@
+//: Playground - noun: a place where people can play
+
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 2
+//declaration
+var fruitNames: [String] = ["Watermelon", "Banana", "Strawberries"]
+//assign fruitNames with 3 of your favorite fruits
+
+//Part 2
+print(fruitNames)
+
+//string interpolation, this is not an interation
+print("I like to eat\(fruitNames)")
+
+for basket in fruitNames
+
+{
+  print("I like to eat" + basket)
+}
+
+//Part 7 #2
+//print "name" at index 1 in fruitNames
+print(fruitNames[1])
+print(fruitNames[0])
+
+
+
+
+
+
+
